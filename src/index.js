74042da71e621ab78e00950efbf213f2e2fd897c
Quan Bao <quy123bao20@gmail.com>
2021-05-18 13:46:30 +0700

    add babel

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-const message = "hello world";
-console.log(message);
+const firstPerson = {
+  name: "John",
+  age: 20,
+  gender: "male",
+  job: "waitress"
+};
+
+const secondPerson = {
+  ...firstPerson,
+  name:"Doe",
+  age: 22,
+}
+
+console.log(firstPerson);
+console.log(secondPerson);
